4c2d0d94dc95f17cd1620127ba666201000b0ad8
Manuel <m.henrichv@googlemail.com>
2021-03-25 19:04:09 +0100

    band_generator opts implemented in DiscreteRsa3D.py

--- dragen/generation/DiscreteRsa3D.py
+++ dragen/generation/DiscreteRsa3D.py
@@ -61,9 +61,10 @@ class DiscreteRsa3D:
         return ellipse, x_0, y_0, z_0
 
     def rsa_plotter(self, array, iterator, attempt):
+        plt.ioff()
         t_0 = datetime.datetime.now()
         n_grains = self.n_grains
-        rve_x, rve_y, rve_z = np.where(array >= 1)
+        rve_x, rve_y, rve_z = np.where((array >= 1) | (array == -200))
         grain_tuples = [*zip(rve_x, rve_y, rve_z)]
         grains_x = [self.x_grid[grain_tuples_i[0]][grain_tuples_i[1]][grain_tuples_i[2]]
                     for grain_tuples_i in grain_tuples]
@@ -74,7 +75,7 @@ class DiscreteRsa3D:
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(grains_x, grains_y, grains_z, c=array[np.where(array > 0)], s=1, vmin=0, vmax=n_grains, cmap='seismic')
+        ax.scatter(grains_x, grains_y, grains_z, c=array[np.where((array > 0) | (array == -200))], s=1, vmin=-20,  vmax=n_grains, cmap='seismic')
         #ax.scatter(grains_x, grains_y, grains_z, c='r', s=1)
 
         ax.set_xlim(-5, self.box_size + 5)
@@ -90,31 +91,46 @@ class DiscreteRsa3D:
         if self.debug:
             self.logger.info('time spent on plotter for grain {}: {}'.format(iterator, time_elapse.total_seconds()))
 
-    def run_rsa(self, animation=False):
+    def run_rsa(self, banded_rsa_array=None, animation=False):
         status = False
-        rsa = np.zeros((2 * self.n_pts, 2 * self.n_pts, 2 * self.n_pts), dtype=np.int32)
-        rsa = self.rve_utils_object.gen_boundaries_3D(rsa)
+        bandratio = 0.3
+
+        if banded_rsa_array is None:
+            rsa = np.zeros((2 * self.n_pts, 2 * self.n_pts, 2 * self.n_pts), dtype=np.int32)
+            rsa = self.rve_utils_object.gen_boundaries_3D(rsa)
+
+        else:
+            rsa = banded_rsa_array
+
+        band_vol_0 = np.count_nonzero(rsa == -200)
+        rsa_boundaries = rsa.copy()
         x_0_list = list()
         y_0_list = list()
         z_0_list = list()
-        rsa_boundaries = rsa.copy()
+
         i = 1
         attempt = 0
         while i < self.n_grains + 1 | attempt < 1000:
             t_0 = datetime.datetime.now()
             free_points_old = np.count_nonzero(rsa == 0)
+            band_points_old = np.count_nonzero(rsa == -200)
             grain = rsa_boundaries.copy()
+            backup_rsa = rsa.copy()
             ellipse, x0, y0, z0 = self.gen_ellipsoid(rsa, iterator=i-1)
-            grain[(ellipse <= 1) & (grain == 0)] = i
+            grain[(ellipse <= 1) & ((grain == 0) | (grain == -200))] = i
             periodic_grain = self.rve_utils_object.make_periodic_3D(grain, ellipse, iterator=i)
-            rsa[(periodic_grain == i) & (rsa == 0)] = i
+            rsa[(periodic_grain == i) & ((rsa == 0) | (rsa == -200))] = i
 
             if animation:
                 self.rsa_plotter(rsa, iterator=i, attempt=attempt)
 
             free_points = np.count_nonzero(rsa == 0)
-            if free_points_old - free_points != np.count_nonzero(periodic_grain):
-                rsa[np.where(rsa == i)] = 0
+            band_points = np.count_nonzero(rsa == -200)
+            if (free_points_old + band_points_old - free_points -band_points != np.count_nonzero(periodic_grain)) | \
+                    (band_points/band_vol_0 < (1-bandratio)):
+                print('difference: ', free_points_old - free_points != np.count_nonzero(periodic_grain))
+                print('ratio:', (band_points/band_vol_0 < bandratio))
+                rsa = backup_rsa.copy()
                 attempt = attempt + 1
 
             else:
@@ -144,7 +160,7 @@ if __name__ == '__main__':
     # Define resolution of Grid
     n_pts_test = 50
     rsa_obj = DiscreteRsa3D(box_size, n_pts_test, a, b, c, debug_test)
-    rsa, x_0_list, y_0_list, z_0_list, status = rsa_obj.run_rsa(animation_test)
+    rsa, x_0_list, y_0_list, z_0_list, status = rsa_obj.run_rsa(np.array, animation_test)
     np.save('./3D_x0_list', x_0_list, allow_pickle=True, fix_imports=True)
     np.save('./3D_y0_list', y_0_list, allow_pickle=True, fix_imports=True)
     np.save('./3D_z0_list', z_0_list, allow_pickle=True, fix_imports=True)
