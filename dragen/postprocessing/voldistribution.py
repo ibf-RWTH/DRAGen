ee82446cf5b9eb5666b4d21635b624bd7878afce
Manuel <m.henrichv@googlemail.com>
2021-05-07 11:12:46 +0200

    fixed bugs in voldistribution.py

--- dragen/postprocessing/voldistribution.py
+++ dragen/postprocessing/voldistribution.py
@@ -3,7 +3,6 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-import csv
 from sklearn.neighbors import KernelDensity
 
 
@@ -22,35 +21,45 @@ class PostProcVol:
     def gen_in_out_lists(self) -> tuple:
 
         discrete_input_df = pd.read_csv(self.output_path+'/Generation_Data/discrete_input_vol.csv')
-        discrete_input_list =  discrete_input_df.to_numpy().flatten().tolist()
+        if self.dimension == 3:
+            discrete_input_df['r_ref'] = np.cbrt(3*discrete_input_df['final_discrete_volume']/(4*np.pi))
+        discrete_input_list = discrete_input_df['r_ref'].to_numpy().flatten().tolist()
 
         discrete_output_df = pd.read_csv(self.output_path + '/Generation_Data/discrete_output_vol.csv')
-        discrete_output_list = discrete_output_df.to_numpy().flatten().tolist()
+        if self.dimension == 3:
+            discrete_output_df['r_ref'] = np.cbrt(3*discrete_output_df['final_discrete_volume']/(4*np.pi))
+        discrete_output_list = discrete_output_df['r_ref'].to_numpy().flatten().tolist()
 
         conti_input_df = pd.read_csv(self.output_path + '/Generation_Data/conti_input_vol.csv')
-        conti_input_list = conti_input_df.to_numpy().flatten().tolist()
+        if self.dimension == 3:
+            conti_input_df['r_ref'] = np.cbrt(3*conti_input_df['final_conti_volume']/(4*np.pi))
+        conti_input_list = conti_input_df['r_ref'].to_numpy().flatten().tolist()
 
         conti_output_df = pd.read_csv(self.output_path + '/Generation_Data/conti_output_vol.csv')
-        conti_output_list = conti_output_df.to_numpy().flatten().tolist()
+        if self.dimension == 3:
+            conti_output_df['r_ref'] = np.cbrt(3*conti_output_df['final_conti_vol']/(4*np.pi))
+        print(conti_output_df)
+        conti_output_list = conti_output_df['r_ref'].to_numpy().flatten().tolist()
+
 
         return discrete_input_list, discrete_output_list, conti_input_list, conti_output_list
 
     def gen_plots(self) -> None:
 
-        kde_disc_in = KernelDensity(bandwidth=10.0, kernel='gaussian')
-        kde_disc_out = KernelDensity(bandwidth=10.0, kernel='gaussian')
-        kde_conti_in = KernelDensity(bandwidth=10.0, kernel='gaussian')
-        kde_conti_out = KernelDensity(bandwidth=10.0, kernel='gaussian')
+        kde_disc_in = KernelDensity(bandwidth=.25, kernel='gaussian')
+        kde_disc_out = KernelDensity(bandwidth=.25, kernel='gaussian')
+        kde_conti_in = KernelDensity(bandwidth=.25, kernel='gaussian')
+        kde_conti_out = KernelDensity(bandwidth=.25, kernel='gaussian')
 
         kde_disc_in.fit(np.reshape(self.discrete_input_list, (-1, 1)))
         kde_disc_out.fit(np.reshape(self.discrete_output_list, (-1, 1)))
         kde_conti_in.fit(np.reshape(self.conti_input_list, (-1, 1)))
-        kde_conti_out.fit(np.reshape(self.conti_input_list, (-1, 1)))
+        kde_conti_out.fit(np.reshape(self.conti_output_list, (-1, 1)))
 
         # capture x-lim
-        max_vol = int(max(self.conti_input_list))
-        max_vol = 500
-        x_d = np.linspace(0, max_vol, 50000)
+        max_x = int(max(self.conti_input_list))
+        max_x = max_x+0.25*max_x
+        x_d = np.linspace(0, max_x, 50000)
 
         # score_samples returns the log of the probability density
         logdens_d_in = kde_disc_in.score_samples(x_d[:, None])
@@ -59,27 +68,40 @@ class PostProcVol:
         logdens_c_out = kde_conti_out.score_samples(x_d[:, None])
 
         ##
-        plt.plot(x_d, np.exp(logdens_c_in), 'r', label='continuous volume input data')
-        plt.plot(x_d, np.exp(logdens_d_in), '--r', label='discrete volume input data')
+        fig = plt.figure(figsize=(11, 8))
 
-        plt.plot(x_d, np.exp(logdens_c_out), 'k', label='continuous volume output data')
-        plt.plot(x_d, np.exp(logdens_d_out), '--k', label='discrete volume output data')
-        if self.dimension == 2:
-            plt.xlabel('Grain Volume [µm²]', fontsize=20)
-        if self.dimension == 3:
-            plt.xlabel('Grain Volume [µm³]', fontsize=20)
-        plt.ylabel('Normalized Density [-] ', fontsize=20)
+        ax = fig.add_subplot(1,1,1)
+
+        ax.set_xlabel('time [s]')
+        ax.set_ylabel('signal')
+
+        ax.plot(x_d, np.exp(logdens_d_in), '--r', label='discrete volume input data')
+        ax.plot(x_d, np.exp(logdens_c_in), 'r', label='continuous volume input data')
+
+        ax.plot(x_d, np.exp(logdens_d_out), '--k', label='discrete volume output data')
+        ax.plot(x_d, np.exp(logdens_c_out), 'k', label='continuous volume output data')
+
+        plt.xlabel('Grain Radius of Reference Sphere (µm)', fontsize=20)
+
+        plt.ylabel('Normalized Density ( - ) ', fontsize=20)
 
         # caputure max y-lim
         max_log_dens = [max(np.exp(logdens_c_in)), max(np.exp(logdens_c_out)),
                         max(np.exp(logdens_d_in)), max(np.exp(logdens_d_out))]
         max_y = max(max_log_dens)
+        max_y = max_y + 0.25*max_y
 
-        plt.xlim(0, max_vol)
+        plt.xlim(0, max_x)
         plt.ylim(0, max_y)
         plt.xticks(fontsize=20)
         plt.yticks(fontsize=20)
         plt.legend(fontsize=12)
-        plt.show()
-        plt.savefig(self.store_path+'/vol_distribution.png', dpi=1200)
+        plt.savefig(self.store_path+'/vol_distribution.png')
 
+if __name__ == "__main__":
+
+    outputPath = 'C:/temp/OutputData/2021-05-06_0'
+    dim_flag = 3
+    obj = PostProcVol(outputPath, dim_flag)
+    obj.gen_in_out_lists()
+    obj.gen_plots()
