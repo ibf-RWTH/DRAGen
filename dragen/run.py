e5c052278150fd92f2a304421c4c68e30dc230f8
Manuel <m.henrichv@googlemail.com>
2021-05-26 11:01:39 +0200

    updated run.py for 2D Mesher

--- dragen/run.py
+++ dragen/run.py
@@ -37,29 +37,25 @@ class Run:
 
     def run(self):
 
-
         # calculate n_pts from box_size and resolution
         n_pts = math.ceil(float(self.box_size) * self.resolution)
         if n_pts % 2 != 0:
             n_pts += 1
-
-        self.infobox_obj.emit("the chosen resolution lead to {}^{} points in the grid".format(str(n_pts),
-                              str(self.dimension)))
-
-
+        if self.infobox_obj is not None:
+            self.infobox_obj.emit("the chosen resolution lead to {}^{} points in the grid".format(str(n_pts),
+                                                                                                  str(self.dimension)))
 
         if self.dimension == 2:
-            obj2D = DataTask2D(box_size=self.box_size, n_pts=n_pts, number_of_bands=self.number_of_bands,
+            obj2D = DataTask2D(box_size=self.box_size, n_pts=int(n_pts), number_of_bands=self.number_of_bands,
                                bandwidth=self.band_width, shrink_factor=self.shrink_factor,
                                band_ratio_rsa=self.band_ratio_rsa, band_ratio_final=self.band_ratio_final,
-                               file1=self.file1,
-                               file2=self.file2,
+                               file1=self.file1, file2=self.file2, phase_ratio=self.phase_ratio,
+                               store_path=self.store_path,
                                gui_flag=True, gan_flag=self.gan_flag, anim_flag=self.visualization_flag)
 
-
-            grains_df = obj2D.initializations(self.dimension)
             for i in range(self.number_of_rves):
-                obj2D.rve_generation(i, grains_df)
+                grains_df, store_path = obj2D.initializations(self.dimension, epoch=i)
+                obj2D.rve_generation(grains_df, store_path)
 
         elif self.dimension == 3:
             obj3D = DataTask3D(box_size=self.box_size, n_pts=int(n_pts), number_of_bands=self.number_of_bands,
@@ -89,50 +85,21 @@ class Run:
             sys.exit()
 
 
-
 if __name__ == "__main__":
-    last_RVE = 1  # Specify the number of iterations
-    dimension = 3
-    # Optional arguments with default values:
-    # obj3D = DataTask3D()
-    obj3D = DataTask3D(box_size=22, n_pts=34, number_of_bands=0, bandwidth=5,
-                       shrink_factor=0.5, band_ratio_rsa=0.75, band_ratio_final=0.75,
-                       file1='../ExampleInput/ferrite_54_grains.csv',
-                       file2='../ExampleInput/pearlite_21_grains.csv',
-                       gui_flag=False, gan_flag=True, anim_flag=False)
+    box_size = 50
+    resolution = 2
+    number_of_rves = 1
+    number_of_bands = 0
+    bandwidth = 5
+    visualization_flag = False
+    file1 = '../ExampleInput/ferrite_54_grains.csv'
+    file2 = '../ExampleInput/Pearlite_21_grains.csv'
+    phase_ratio = 0.5
+    store_path = '../'
+    shrink_factor = 0.5
+    dimension = 2
 
-    obj2D = DataTask2D(box_size=20, n_pts=40, number_of_bands=1, bandwidth=5,
-                       shrink_factor=0.5, band_ratio_rsa=0.75, band_ratio_final=0.75,
-                       file1='../ExampleInput/ferrite_54_grains.csv',
-                       file2='../ExampleInput/pearlite_21_grains.csv',
-                       gui_flag=False, gan_flag=True, anim_flag=True)
-
-    try:
-        if dimension == 2:
-            grains_df = obj2D.initializations(dimension)
-            for i in range(last_RVE):
-                obj2D.rve_generation(i, grains_df)
-            sys.exit()
-        elif dimension == 3:
-            for i in range(last_RVE):
-                grains_df, store_path = obj3D.initializations(dimension, epoch=i)
-                obj3D.rve_generation(grains_df, store_path)
-                PostProcVol_obj = PostProcVol(store_path, dim_flag=3)
-                PostProcVol_obj.gen_in_out_lists()
-                PostProcVol_obj.gen_plots()
-            sys.exit()
-        else:
-            LOGS_DIR = 'Logs/'
-            logger = logging.getLogger("RVE-Gen")
-            if not os.path.isdir(LOGS_DIR):
-                os.makedirs(LOGS_DIR)
-            f_handler = logging.handlers.TimedRotatingFileHandler(
-                filename=os.path.join(LOGS_DIR, 'dragen-logs'), when='midnight')
-            formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
-            f_handler.setFormatter(formatter)
-            logger.addHandler(f_handler)
-            logger.setLevel(level=logging.DEBUG)
-            logger.info('dimension must be 2 or 3')
-            sys.exit()
-    except KeyboardInterrupt:
-        print('Exiting!!!')
+    Run(box_size, resolution, number_of_rves, number_of_bands, bandwidth,
+        dimension, visualization_flag, file1, file2,
+        phase_ratio, store_path, shrink_factor, band_ratio_rsa=0.75, band_ratio_final=0.75,
+        gui_flag=False, gan_flag=False, info_box_obj=None, progress_obj=None).run()
