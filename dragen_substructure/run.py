4a36ddbfe2a643116b961a862bef9b008a34efe7
lhkgit <linghao.kongrwth@gmail.com>
2021-09-26 15:44:49 +0200

    clean up;gen from file or user define

--- dragen_substructure/run.py
+++ dragen_substructure/run.py
@@ -6,70 +6,104 @@ Version:  V 0.1
 File:     run
 Describe: Write during the internship at IEHK RWTH"""
 import pandas as pd
-from dragen_substructure.substructure import Grain,rve_substruct_plotter
+from substructure.rve_substructure import rve_substruct_plotter
 import numpy as np
-from dragen_substructure.data import save_data
+from substructure.data import save_data
 import logging
 import logging.handlers
 import os
+from dragen_substructure.substructure import Grain
 
 class Run():
 
-    def __init__(self,box_size,n_pts,root_dir,a = 2,b = 2,c = 3,x_0 = 0,y_0 = 0,z_0 = 0,grainID = None,OR='KS'):
+    def __init__(self,box_size, n_pts,equiv_d=None, p_sigma=None, t_mu=None, b_sigma=0.001, decreasing_factor=0.95,
+                 lower=None, upper=None, circularity=1, plt_name=None, save=True, plot=False,
+                 filename=None, fig_path=None, gen_flag='user_define',block_file=None, OR='KS'):
 
         self.box_size = box_size
         self.n_pts = n_pts
-        self.a = a
-        self.b = b
-        self.c = c
-        self.x_0 = x_0
-        self.y_0 = y_0
-        self.z_0 = z_0
-        self.grainID = grainID
+        self.equiv_d = equiv_d
+        self.p_sigma = p_sigma
+        self.t_mu = t_mu
+        self.b_sigma = b_sigma
+        self.decreasing_factor = decreasing_factor
+        self.lower = lower
+        self.upper = upper
+        self.circularity = circularity
+        self.plt_name = plt_name
+        self.save = save
+        self.plot = plot
+        self.filename = filename
+        self.fig_path = fig_path
         self.OR = OR
-        self.logger = logging.getLogger('RVE-substruct')
-        self.root_dir = root_dir
+        self.block_file = block_file
+        self.gen_flag = gen_flag
 
-    def setup_logging(self):
+    def get_orientations(self,block_df, grainID):
+        blocks = block_df[block_df['grain_id'] == grainID+1] # +1...
+        n_pack = len(list(set(blocks['packet_id'])))
+        groups = blocks.groupby('packet_id')
 
-        LOGS_DIR = self.root_dir + '/Logs/'
-        if not os.path.isdir(LOGS_DIR):
-            os.makedirs(LOGS_DIR)
-        f_handler = logging.handlers.TimedRotatingFileHandler(
-            filename=os.path.join(LOGS_DIR, 'dragen-logs'), when='midnight')
-        formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
-        f_handler.setFormatter(formatter)
-        self.logger.addHandler(f_handler)
-        self.logger.setLevel(level=logging.DEBUG)
+        ori_dflist = []
+        for name, group in groups:
+            ori_df = group[['phi1', 'PHI', 'phi2']]
+            ori_dflist.append(ori_df)
 
-    def run(self,rve_df,grains_df,equiv_d, mu, sigma,lower=None,upper=None,plt_name = None,save= True,plot=False,store_path = None,filename = None,fig_path = None):
+        n_pack = np.arange(n_pack)
 
-        self.setup_logging()
-        self.logger.info('------------------------------------------------------------------------------')
-        self.logger.info('substructure generation begins')
-        self.logger.info('------------------------------------------------------------------------------')
+        n_pack_to_ori = dict(zip(n_pack, ori_dflist))
+
+        return n_pack_to_ori
+
+    def get_bt_distribution(self,block_df):
+
+        average_bt = block_df['block_thickness'].mean()
+        return average_bt
+
+    def run(self,rve_df,grains_df,store_path,logger):
+
+        logger.info('------------------------------------------------------------------------------')
+        logger.info('substructure generation begins')
+        logger.info('------------------------------------------------------------------------------')
         rve_data = pd.DataFrame()
+        if self.gen_flag == 'from_file':
+            assert self.block_file is not None
+            block_df = pd.read_csv(self.block_file)
+            average_bt = self.get_bt_distribution(block_df)
+            average_bt = self.decreasing_factor * average_bt
 
         for i in range(len(grains_df)):
 
             grain_data = grains_df.iloc[i]
             phaseID = int(grain_data['phaseID'])
             grain_id = grain_data['GrainID']
-            x = rve_df[rve_df['GrainID'] == grain_id + 1]['x'].to_numpy().reshape((-1, 1)) #needs + 1 finally
-            y = rve_df[rve_df['GrainID'] == grain_id + 1]['y'].to_numpy().reshape((-1, 1))
-            z = rve_df[rve_df['GrainID'] == grain_id + 1]['z'].to_numpy().reshape((-1, 1))
+            x = rve_df[rve_df['GrainID'] == grain_id+1]['x'].to_numpy().reshape((-1, 1)) #needs + 1 finally
+            y = rve_df[rve_df['GrainID'] == grain_id+1]['y'].to_numpy().reshape((-1, 1))
+            z = rve_df[rve_df['GrainID'] == grain_id+1]['z'].to_numpy().reshape((-1, 1))
 
             points = np.concatenate((x, y, z), axis=1)
 
             if phaseID == 2:
 
                 orientation = (grain_data['phi1'], grain_data['PHI'], grain_data['phi2'])
-                grain = Grain(box_size=self.box_size, n_pts=self.n_pts, v=grain_data['final_conti_volume'], points=points,
+                grain = Grain(v=grain_data['final_conti_volume'], points=points,
                               phaseID=phaseID,grainID=grain_id,orientation=orientation)
 
-                grain.gen_packets(equiv_d)
-                grain.gen_blocks(mu, sigma,lower=lower,upper=upper)
-                grain.block_orientation_assignment()
+                if self.gen_flag == 'from_file':
+                    old_gid = grain_data['old_gid']
+                    blocks = block_df[block_df['grain_id'] == old_gid + 1]
+                    n_pack = len(list(set(blocks['packet_id'])))
+                    orientations = self.get_orientations(block_df, old_gid)
+                    grain.gen_subs(block_thickness=average_bt, b_sigma=self.b_sigma, lower_t=self.lower, upper_t=self.upper,
+                                   circularity=self.circularity,
+                                   n_pack=n_pack, orientations=orientations)
+
+                if self.gen_flag == 'user_define':
+                    assert self.equiv_d is not None
+                    assert self.p_sigma is not None
+                    assert self.t_mu is not None
+                    grain.gen_subs(self.equiv_d,sigma=self.p_sigma,block_thickness=self.t_mu,b_sigma=self.b_sigma,lower_t=self.lower,
+                                   upper_t=self.upper,circularity=self.circularity)
                 rve_data = pd.concat([rve_data,grain.points_data])
 
             else:
@@ -96,31 +130,36 @@ class Run():
 
         rve_data['packet_id'] = pid_in_rve
         rve_data['block_id'] = bid_in_rve
+        rve_data['n_pts'] = self.n_pts
+        rve_data['box_size'] = self.box_size
 
-        if save:
+        if self.save:
 
-            if filename:
+            if self.filename:
 
-                save_data(rve_data,store_path,filename)
+                save_data(rve_data,store_path,self.filename)
 
             else:
-
                 save_data(rve_data,store_path)
 
-        if plot:
+        if self.plot:
 
-            for name in plt_name:
+            for name in self.plt_name:
 
-                rve_substruct_plotter(rve_data,name,fig_path)
+                rve_substruct_plotter(rve_data,name,self.fig_path)
 
-        self.logger.info('substructure generation successful')
-        self.logger.info('------------------------------------------------------------------------------')
+        logger.info('substructure generation successful')
+        logger.info('------------------------------------------------------------------------------')
 
         return rve_data
 
 if __name__ == '__main__':
 
-    test_run = Run(30,60,'F:/pycharm/2nd_mini_thesis/substructure')
+    import datetime
+    start = datetime.datetime.now()
+    test_run = Run(20,40,'F:/pycharm/2nd_mini_thesis/substructure')
     rve_data = pd.read_csv('F:/pycharm/2nd_mini_thesis/dragen-master/OutputData/2021-07-23_0/substruct_data_abq.csv') #changes into rve data
     grains_df = pd.read_csv('F:/pycharm/2nd_mini_thesis/dragen-master/OutputData/2021-07-23_0/Generation_Data/grain_data_input.csv')#grain_data
-    test_run.run(rve_data,grains_df,6.8,0.5,0.01,store_path='./',plt_name=['packet','block'],plot=True,fig_path='D:/')
+    test_run.run()
+    end = datetime.datetime.now()
+    print('running time is',end-start)
