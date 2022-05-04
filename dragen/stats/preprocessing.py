8882e02a1d80d0310a4b8d2898f7731efe64a70b
lhkgit <linghao.kongrwth@gmail.com>
2022-05-04 17:08:13 +0200

    add comments

--- dragen/stats/preprocessing.py
+++ dragen/stats/preprocessing.py
@@ -4,7 +4,8 @@ Time:     2022/5/3 10:22
 Author:   Linghao Kong
 Version:  V 0.1
 File:     sample
-Describe: Writing at home"""
+Describe: Writing for DRAGen"""
+
 from sklearn.neighbors import KernelDensity
 from sklearn.model_selection import KFold,GridSearchCV
 import numpy as np
@@ -13,12 +14,18 @@ import matplotlib.pyplot as plt
 from scipy.stats import lognorm
 
 class Sampler:
+    '''
+    base Sampler class: markov_matrix may be introduced in the future to increase sampling efficiency,
+    so far it is set to be None in childclass.
+    '''
     def __init__(self,markov_matrix):
         self.markov_matrix = markov_matrix
 
-    '''use acceptance-rejection sampling to achieve complex sampling'''
     @classmethod
     def rejection_sample(cls,intervals:list,pdf,c):
+        '''
+        use acceptance-rejection sampling to achieve complex sampling
+        '''
         while True:
             x = np.random.uniform(intervals[0], intervals[1])
             y = np.random.uniform(0,c)
@@ -27,7 +34,9 @@ class Sampler:
                 return x
 
 class InputDataSampler(Sampler):
-    '''fit the real distribution using KDE and sample from fitted distribution'''
+    '''
+    fit the real distribution in the data using KDE and sample from fitted distribution
+    '''
     def __init__(self,data):
         super(InputDataSampler,self).__init__(None)
         self.data = data
@@ -35,6 +44,10 @@ class InputDataSampler(Sampler):
         self.intervals = [0, 0]#avoid future warning
 
     def _train(self):
+        '''
+        fit the real distribution in the data using KDE. Use GridSearch method to find the most optimal
+        bandwidth for KDE
+        '''
         kFold = KFold(n_splits=10)  # k-folder cross-validation split data into 10 folds
         bandwidths = 10 ** np.linspace(-1, 1, 100)
         grid = GridSearchCV(estimator=KernelDensity(kernel="gaussian"),
@@ -45,26 +58,31 @@ class InputDataSampler(Sampler):
         return kde
 
     def pdf(self, x):
+        '''
+        probability density function for fitted distribution, return the density of the input x
+        '''
         return np.exp(self.kde.score_samples(np.array([x]).reshape(-1, 1)))
 
     def sample(self,intervals:list):
+        '''
+        intervals: sample data from this invertal in the fitted distribution
+        '''
         if intervals[0] != self.intervals[0] or intervals[1] != self.intervals[1]:
             u = np.linspace(intervals[0], intervals[1], 1000)
             self.intervals = u
         else:
             u = self.intervals
-        #transfer uniform distribution to the needed one by acceptance-rejection sampling
-        #1.compute c
         log_dens = self.kde.score_samples(u.reshape(-1,1))
         c = np.max(np.exp(log_dens))*1.1
-        #sample from uniform distribution in intervals
         x = super().rejection_sample(intervals = intervals,
                                      pdf = self.pdf,
                                      c = c)
         return x
 
 class UserBlockThicknessSampler(Sampler):
-    '''produce lognorm distribution'''
+    '''
+    produce lognorm distribution based on user input: average_bt(average block thickness), sigma(standard variance)
+    '''
     def __init__(self, average_bt, sigma = 0.1):
         super(UserBlockThicknessSampler,self).__init__(None)
         self.average_bt = average_bt
@@ -72,15 +90,20 @@ class UserBlockThicknessSampler(Sampler):
         self.intervals = [0, 0]
 
     def pdf(self, x):
+        '''
+        probability density function of produced lognorm distribution
+        '''
         return lognorm.pdf(x, s=self.sigma, scale=self.average_bt)
 
     def sample(self,intervals:list):
+        '''
+        intervals: sample data from this invertal in the produced distribution
+        '''
         if intervals[0] != self.intervals[0] or intervals[1] != self.intervals[1]:
             u = np.linspace(intervals[0], intervals[1], 1000)
             self.intervals = u
         else:
             u = self.intervals
-        u = np.linspace(intervals[0], intervals[1], 1000)
         dens = lognorm.pdf(u,s = self.sigma, scale = self.average_bt)
         c = np.max(dens)*1.1
         x = super().rejection_sample(intervals = intervals,
@@ -89,7 +112,9 @@ class UserBlockThicknessSampler(Sampler):
         return x
 
 class UserPakVolumeSampler(Sampler):
-    '''produce lognorm distribution'''
+    '''
+    produce lognorm distribution
+    '''
     def __init__(self,equiv_d,circularity = 1,sigma = 0.1):
         super(UserPakVolumeSampler,self).__init__(None)
         self.equiv_d = equiv_d
@@ -99,9 +124,15 @@ class UserPakVolumeSampler(Sampler):
         self.intervals = [0 ,0]
 
     def pdf(self,x):
+        '''
+        probability density function of produced lognorm distribution
+        '''
         return lognorm.pdf(x, s=self.sigma, scale=self.average_volume)
 
     def sample(self, intervals:list):
+        '''
+        intervals: sample data from this invertal in the produced distribution
+        '''
         if intervals[0] != self.intervals[0] or intervals[1] != self.intervals[1]:
             u = np.linspace(intervals[0], intervals[1], 1000)
             self.intervals = u
@@ -115,12 +146,26 @@ class UserPakVolumeSampler(Sampler):
         return x
 
 class SamplerFactory:
+    '''
+    Factory Design Pattern: avoid tons of if-else, produce sampler objects depending on passed parameters
+    '''
     def __init__(self, type):
+        '''
+        type: the type of samplers that being produced
+        '''
         self.type = type
     def create_sampler(self,sampler:[InputDataSampler,UserPakVolumeSampler,UserBlockThicknessSampler], **kwargs):
+        '''
+        sampler: must be one of the following samplers: InputDataSampler,UserPakVolumeSampler,UserBlockThicknessSampler.
+        kwargs: the needed parameters for above mentioned 3 samplers. For example, the kwargs for UserBlockThicknessSampler
+        should be average_bt and sigma(defaulted 0.1)
+        '''
         return sampler(**kwargs)
 
 def test_inputsampler():
+    '''
+    test function: test the InputDataSampler, the user usually needs to change the file path
+    '''
     df = pd.read_csv("F:/pycharm/dragen/ExampleInput/example_pag_inp2.csv")
     data = df["volume"].to_numpy().reshape((-1,1))
     data = np.sort(data,axis=0)
@@ -149,6 +194,9 @@ def test_inputsampler():
     plt.show()
 
 def test_usersampler():
+    '''
+    test function: test the UserBlockThicknessSampler or UserPakVolumeSampler, the user usually needs to change the file path and the kind of sampler
+    '''
     pak_sampler = UserPakVolumeSampler(2)
     data = []
     for i in range(100):
