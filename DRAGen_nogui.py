ff3bbf3022f7ddfd1df671800d653fbfd3a8c33e
Manuel David Henrich <manuel.henrich@rwth-aachen.de>
2022-07-05 15:00:19 +0200

    introduced xfem_flag in DRAGen_nogui.py

--- DRAGen_nogui.py
+++ DRAGen_nogui.py
@@ -1,9 +1,9 @@
 from dragen.run import Run
-dimension = 3
-box_size = 100
+dimension = 2
+box_size = 200
 box_size_y = None  # if this is None it will be set to the main box_size value
-box_size_z = 100  # for sheet rve set z to None and y to different value than x the other way round is buggy
-resolution = 0.1
+box_size_z = None  # for sheet rve set z to None and y to different value than x the other way round is buggy
+resolution = 1
 number_of_rves = 1
 smoothing_flag = True
 # Banding Params
@@ -27,11 +27,11 @@ slope_offset = 0
 #
 #
 ###NO30
-file1 = r'F:/OCAS/NO30_RVE_data/Data_processed.csv'
-file2 = r"D:\2nd mini-thesis\dragen\ExampleInput\example_pag_inp.csv"
+#file1 = r'F:/OCAS/NO30_RVE_data/Data_processed.csv'
+#file2 = r"D:\2nd mini-thesis\dragen\ExampleInput\example_pag_inp.csv"
 #DP800
-#file1 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\DP800/TrainedData_Ferrite.pkl'
-#file2 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\DP800/TrainedData_Martensite.pkl'
+file1 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\DP800/TrainedData_Ferrite.pkl'
+file2 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\DP800/TrainedData_Martensite.pkl'
 
 #Bainite
 #PAGs
@@ -39,29 +39,30 @@ file2 = r"D:\2nd mini-thesis\dragen\ExampleInput\example_pag_inp.csv"
 #Blocks
 
 #Inclusions
-file5 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\Inclusions/Inclusions_DRAGen_Input.csv'
+#file5 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\Inclusions/Inclusions_DRAGen_Input.csv'
 
 #Bands
 #file6 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\DP800/TrainedData_Martensite.pkl'
-file6 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\DP800/TrainedData_Ferrite.pkl'
+#file6 = r'E:\Sciebo\IEHK\Publications\ComputationalSci\DRAGen\matdata\DP800/TrainedData_Ferrite.pkl'
 
 # test pearlite phase
-subs_flag = True
+subs_flag = False
 subs_file = './ExampleInput/example_block_inp.csv'
-subs_file_flag = True
+subs_file_flag = False
 gan_flag = False
-moose_flag = True
+moose_flag = False
 abaqus_flag = True
-pbc_flag = True
-submodel_flag = False
-damask_flag = True
+pbc_flag = False
+submodel_flag = True
+damask_flag = False
 phase2iso_flag = True
+x_fem_flag = True
 element_type = 'HEX8'
 anim_flag = False
 
-files = {1: None, 2: file2, 3: None, 4: None, 5: None, 6: None}  # , 2: file2, 6: file6}
-phase_ratio = {1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
-phases = ['Ferrite']  # , 'Martensite', 'Bands']
+files = {1: file1, 2: file2, 3: None, 4: None, 5: None, 6: None}  # , 2: file2, 6: file6}
+phase_ratio = {1: 0.8, 2: 0.2, 3: 0, 4: 0, 5: 0, 6: 0}
+phases = ['Ferrite', 'Martensite'] #, 'Bands']
 
 upper = None
 lower = None
@@ -83,12 +84,12 @@ Number 5 specifies the inclusions and number 6 the Band phase. Either .csv or .p
 Run(dimension=dimension, box_size=box_size, box_size_y=box_size_y, box_size_z=box_size_z, resolution=resolution,
     number_of_rves=number_of_rves, slope_offset=slope_offset, abaqus_flag=abaqus_flag, damask_flag=damask_flag,
     moose_flag=moose_flag, element_type=element_type, pbc_flag=pbc_flag, submodel_flag=submodel_flag,
-    phase2iso_flag=phase2iso_flag, smoothing_flag=smoothing_flag, gui_flag=False, anim_flag=anim_flag,
+    phase2iso_flag=phase2iso_flag, x_fem_flag=x_fem_flag, smoothing_flag=smoothing_flag, gui_flag=False, anim_flag=anim_flag,
     visualization_flag=visualization_flag, root=root, info_box_obj=None, progress_obj=None, phase_ratio=phase_ratio,
     file_dict=files, phases=phases, number_of_bands=number_of_bands, upper_band_bound=upper_band_bound,
     lower_band_bound=lower_band_bound, band_orientation=band_orientation, band_filling=band_filling,
     inclusion_flag=inclusion_flag, inclusion_ratio=inclusion_ratio, subs_flag=subs_flag, subs_file_flag=subs_file_flag,
     subs_file=subs_file, equiv_d=equiv_d, p_sigma=p_sigma, t_mu=t_mu, b_sigma=b_sigma,
     decreasing_factor=decreasing_factor, lower=lower, upper=upper, circularity=circularity, plt_name=plt_name,
-    save=save, plot=plot, filename=filename, fig_path=fig_path, orientation_relationship=orientation_relationship).run()
+    save=save, plot=plot, filename=filename, orientation_relationship=orientation_relationship).run()
 
