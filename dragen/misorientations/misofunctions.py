684c57bf7b3620f79e7279301862b6b95bc6eb31
Nikitas-Vasileios_Georgiou <107918437+nikitasgeor@users.noreply.github.com>
2022-10-05 12:55:38 +0200

    New misorientation calculation function based on damask module implemented

--- dragen/misorientations/misofunctions.py
+++ dragen/misorientations/misofunctions.py
@@ -1,102 +1,5 @@
 import numpy as np
-from numpy.linalg import inv
-from scipy.spatial.transform import Rotation as R
-
-T = np.array([[[1, 0, 0],
-               [0, 1, 0],
-               [0, 0, 1]],
-
-              [[0, 0, 1],
-               [1, 0, 0],
-               [0, 1, 0]],
-
-              [[0, 1, 0],
-               [0, 0, 1],
-               [1, 0, 0]],
-
-              [[0, -1, 0],
-               [0, 0, 1],
-               [-1, 0, 0]],
-
-              [[0, -1, 0],
-               [0, 0, -1],
-               [1, 0, 0]],
-
-              [[0, 1, 0],
-               [0, 0, -1],
-               [-1, 0, 0]],
-
-              [[0, 0, -1],
-               [1, 0, 0],
-               [0, -1, 0]],
-
-              [[0, 0, -1],
-               [-1, 0, 0],
-               [0, 1, 0]],
-
-              [[0, 0, 1],
-               [-1, 0, 0],
-               [0, -1, 0]],
-
-              [[-1, 0, 0],
-               [0, 1, 0],
-               [0, 0, -1]],
-
-              [[-1, 0, 0],
-               [0, -1, 0],
-               [0, 0, 1]],
-
-              [[1, 0, 0],
-               [0, -1, 0],
-               [0, 0, -1]],
-
-              [[0, 0, -1],
-               [0, -1, 0],
-               [-1, 0, 0]],
-
-              [[0, 0, 1],
-               [0, -1, 0],
-               [1, 0, 0]],
-
-              [[0, 0, 1],
-               [0, 1, 0],
-               [-1, 0, 0]],
-
-              [[0, 0, -1],
-               [0, 1, 0],
-               [1, 0, 0]],
-
-              [[-1, 0, 0],
-               [0, 0, -1],
-               [0, -1, 0]],
-
-              [[1, 0, 0],
-               [0, 0, -1],
-               [0, 1, 0]],
-
-              [[1, 0, 0],
-               [0, 0, 1],
-               [0, -1, 0]],
-
-              [[-1, 0, 0],
-               [0, 0, 1],
-               [0, 1, 0]],
-
-              [[0, -1, 0],
-               [-1, 0, 0],
-               [0, 0, -1]],
-
-              [[0, -1, 0],
-               [1, 0, 0],
-               [0, 0, 1]],
-
-              [[0, 1, 0],
-               [1, 0, 0],
-               [0, 0, -1]],
-
-              [[0, 1, 0],
-               [-1, 0, 0],
-               [0, 0, 1]]])
+import damask
 
 def pairs2d(grid):
     npairs = np.empty((0, 2))
@@ -260,81 +163,31 @@ def pairs3d(grid):
                 print(len(pairs))
     return pairs
 
+def calc_miso(grains,pairs,degrees):
+    quaternions=np.empty((0,4))
+    angle=np.empty((0,1))
 
-def orientation_matrix(x, degrees):
-    if degrees == True:
-        x = (np.radians(x[0]), np.radians(x[1]), np.radians(x[2]))
-    else:
-        pass
+    for i in range(0, len(pairs)):
+        x = int(pairs[i, 0])
+        y = int(pairs[i, 1])
+        o1 = np.array([grains[x - 1, 4], grains[x - 1, 5], grains[x - 1, 6]])
+        o2 = np.array([grains[y - 1, 4], grains[y - 1, 5], grains[y - 1, 6]])
 
-    cf1 = np.cos(x[0])
+        if degrees==True:
+            a = damask.Orientation.from_Euler_angles(phi=o1, degrees=True, family='cubic')
+            b = damask.Orientation.from_Euler_angles(phi=o2, degrees=True, family='cubic')
+        else:
+            a = damask.Orientation.from_Euler_angles(phi=o1, degrees=False, family='cubic')
+            b = damask.Orientation.from_Euler_angles(phi=o2, degrees=False, family='cubic')
 
-    sf1 = np.sin(x[0])
 
-    cF = np.cos(x[1])
+        c = a.disorientation(b)
+        a_a=damask.Orientation.as_axis_angle(c,degrees=True,pair=True)
+        a=a_a[1]
+        quaternions = np.append(quaternions, [c], 0)
+        angle=np.append(angle,[[a]],0)
+        cond=((len(quaternions) / len(pairs)) * 100)
+        print("Completion: "+str(cond))
 
-    sF = np.sin(x[1])
+    return quaternions,angle
 
-    cf2 = np.cos(x[2])
-
-    sf2 = np.sin(x[2])
-
-    ori_matrix = np.array([[cf1 * cf2 - sf1 * sf2 * cF, sf1 * cf2 + cf1 * sf2 * cF, sf2 * sF],
-                           [-cf1 * sf2 - sf1 * cf2 * cF, -sf1 * sf2 + cf1 * cf2 * cF, cf2 * sF],
-                           [sf1 * sF, -cf1 * sF, cF]])
-    return ori_matrix
-
-def misorientation(x, y, degrees, grains):
-    '''
-    function that calculates the misorientation angle between two grains
-    Input:
-        x=list, [phi1,PHI,ph2]
-        y=list, [phi1,PHI,ph2]
-
-    Output:
-        misorientation angle in degrees
-    '''
-    x = (grains[x-1, 4], grains[x-1, 5], grains[x-1, 6])
-    y = (grains[y-1, 4], grains[y-1, 5], grains[y-1, 6])
-
-    print(x)
-    print(y)
-
-    if degrees == True:
-        rot = (orientation_matrix(x, degrees=True))
-        rot1 = (orientation_matrix(y, degrees=True))
-    else:
-        rot = (orientation_matrix(x, degrees=False))
-        rot1 = (orientation_matrix(y, degrees=False))
-    # print(rot)
-    # print(rot1)
-    Mis = np.matmul(rot, inv(rot1))
-    # print(Mis)
-    M = np.empty((0, 3, 3))
-    a = np.empty(0)
-
-    for i in range(0, 24):
-        Mi = np.matmul(T[i], Mis)
-        M = np.append(M, [Mi], axis=0)
-        y = ((Mi.trace() - 1) / 2)
-        # print(y)
-        ai = np.arccos(y)
-        ai = np.degrees(ai)
-        a = np.append(a, [ai], axis=0)
-
-    angle = np.min(a)
-    h = np.where(angle == a)
-    matrix = M[h]
-    #print(matrix)
-    # print(matrix)
-
-    denominator = np.sqrt((matrix[0, 1, 2] - matrix[0, 2, 1]) ** 2 + (matrix[0, 2, 0] - matrix[0, 0, 2]) ** 2 + (
-                matrix[0, 0, 1] - matrix[0, 1, 0]) ** 2)
-    # denominator=2*np.sin(angle)
-    r1 = (matrix[0, 1, 2] - matrix[0, 2, 1]) / denominator
-
-    r2 = (matrix[0, 2, 0] - matrix[0, 0, 2]) / denominator
-    r3 = (matrix[0, 0, 1] - matrix[0, 1, 0]) / denominator
-
-    axis = (r1, r2, r3)
-    return angle, axis
