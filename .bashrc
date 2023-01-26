6dc5b312d3c6912a6c8acdaf64e4fec3951fd191
RWTHLHK <klh8866@163.com>
2023-01-26 11:01:02 +0100

    add compare distribution result

---
+++ .bashrc
@@ -0,0 +1,1 @@
+export LD_LIBRARY_PATH=/home/doelz-admin/Qt/5.15.2/gcc_64/lib:$LD_LIBRARY_PATH
--- dragen/test/compare_distribution.py
+++ dragen/test/compare_distribution.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import pandas as pd
 from sklearn.neighbors import KernelDensity
 from sklearn.model_selection import KFold, GridSearchCV
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     # input data
     sub_data = pd.read_csv(
-        r"O:\Werkstoffmechanik\01 WM MMD\29 Linghao\DRAGen\DRAGen\ExampleInput\example_block_inp.csv")
+        r"/home/doelz-admin/projects/SubstructJulia/DRAGen/ExampleInput/example_block_inp.csv")
     print(sub_data)
     kFold = KFold(n_splits=10)  # k-folder cross-validation split data into 10 folds
     bandwidths = 10 ** np.linspace(-1, 1, 100)
@@ -23,16 +24,22 @@ if __name__ == "__main__":
     xs = np.sort(xs)
     xs = xs.reshape(-1, 1)
     ys = np.exp(estimator.score_samples(xs))
-
+    print("finish fitting input distribution")
     #fit generated distribution
-    data = pd.read_csv(r"O:\Werkstoffmechanik\01 WM MMD\29 Linghao\DRAGen\DRAGen\OutputData\2023-01-12_000\substruct_data.csv")
+    data = pd.read_csv(r"dragen/test/substruct_data.csv")
+    data = data.groupby("block_id").head()
+   
     bts2 = data['block_thickness'].to_numpy().reshape(-1, 1)
     grid.fit(bts2)
+    print("finish fitting generated distribution")
     estimator2 = grid.best_estimator_
     xs2 = np.linspace(data['block_thickness'].min(), data['block_thickness'].max(), 100)
     xs2 = np.sort(xs2)
     xs2 = xs2.reshape(-1, 1)
     ys2 = np.exp(estimator.score_samples(xs2))
-    plt.plot(xs2, ys2)
-    plt.plot(xs, ys)
+    plt.plot(xs2, ys2,label="generated block thickness distribution")
+    plt.plot(xs, ys,label="input block thickness distribution")
+    plt.xlabel("Block Thickness")
+    plt.ylabel("Density")
+    plt.legend()
     plt.show()
